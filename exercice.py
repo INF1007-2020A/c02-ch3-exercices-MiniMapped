6582131b92a220465e97e37500e0e6a700d19ce8
MiniMapped <marc-alexandre.parent@polymtl.ca>
2020-09-14 13:50:43 -0400

    exercices 3 et 4 faits

--- exercice.py
+++ exercice.py
@@ -13,25 +13,42 @@ def orthogonal(v1, v2):
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	sum = 0
+	sum_values = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v >= 0:
+			sum += v
+			sum_values += 1
+	return (sum / sum_values)
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	twenties = tens = fives = twos = ones = 0
 	while value != 0:
 		if value >= 20:
+			twenties = value//20
+			value -= (twenties*20)
 			pass
 		elif value >= 10:
+			tens = value // 10
+			value -= (tens * 10)
 			pass
 		elif value >= 5:
+			fives = value // 5
+			value -= (fives * 5)
 			pass
 		elif value >= 1:
+			twos = value // 2
+			value -= (twos * 2)
+			if value == 1:
+				ones += 1
+				value = 0
 			pass
 
 	return (twenties, tens, fives, twos, ones);
 
 if __name__ == "__main__":
-	print(dissipated_power(69, 420))
-	print(orthogonal((1, 1), (-1, 1)))
-	print(average([1, 4, -2, 10]))
-	print(bills(137))
+	#print(dissipated_power(69, 420))
+	#print(orthogonal((1, 1), (-1, 1)))
+	#print(average([1, 4, -2, 10]))
+	print(bills(138))
